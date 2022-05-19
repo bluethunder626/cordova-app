830af3779c0bc69af5fba603d4630fb1f098cf43
Francis M <>
2022-05-19 09:24:56 +0800

    first commit

---
+++ www/build/blue-components.esm.js
@@ -0,0 +1,1 @@
+import{p as t,b as m}from"./p-0c873dd4.js";(()=>{const m=import.meta.url,o={};return""!==m&&(o.resourcesUrl=new URL(".",m).href),t(o)})().then((t=>m([["p-2fc24504",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)));
